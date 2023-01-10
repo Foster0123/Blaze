a086990ca852f59ff395a103df19e64b901b9ca8
Foster <72973015+Foster0123@users.noreply.github.com>
2023-01-11 01:36:09 +0530

    Blaze Components In Development

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -1,45 +1,28 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function NavbarModule(props: any) {
-  return (
-    <>
-      <Navbar expand="lg" className='navbar'>
-        <Container fluid>
-          <Navbar.Brand href="/">
-            <img
-              src={props.logoImage}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Blaze Logo"
-            />
-            Blaze
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Todos</Nav.Link>
-              <Nav.Link href="#">Sign In</Nav.Link>
-              <Nav.Link href="#">Sign Up</Nav.Link>
-              <Nav.Link href="#"></Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-        <div className='profile-container'>
-          <img
-            src={props.logoImage}
-            width="40"
-            height="40"
-            className="profile-image"
-            alt="Blaze Logo"
-          />
-        </div>
-      </Navbar>
-    </>
-  );
+	return (
+		<>
+			<nav className="navbar">
+				<div className="navbar-header">
+					<img src={props.logoImage} width="50" height="50" alt="Blaze Logo" />
+					<header className="nav-title">Blaze</header>
+				</div>
+				<div>
+					<ul className="nav-link-container">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Todos</a></li>
+						<li><a href="#">Sign In</a></li>
+						<li><a href="#">Sign Up</a></li>
+						<li><a href="#">About</a></li>
+					</ul>
+					<ul className="nav-profile-container">
+						<li><img src={props.logoImage} alt="Profile Picture" /></li>
+						<li><button><i className="fa-solid fa-bars"></i></button></li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
 }
 
 export default NavbarModule;
