0affb17821158082b59f02399f2d919a4f74919c
Foster <72973015+Foster0123@users.noreply.github.com>
2023-02-08 22:26:38 +0530

    Blaze Signup Development In Progress

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,10 +1,68 @@
+import { Stack, InputGroup, InputLeftAddon, InputRightAddon, Input, Checkbox, Button, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import NavBar from "./shared/NavBar";
 
 const Signup = () => {
-    return(
+    return (
         <div>
             <NavBar />
-            <h1>Signup</h1>
+            <div className="signin-form-container">
+                <form action="" className="signin-form">
+                    <Stack>
+
+                        <Text fontSize="4xl" textAlign="center">Sign In</Text>
+
+                        <InputGroup>
+                            <InputLeftAddon style={{ paddingLeft: "5px", paddingRight: "5px" }}>
+                                <i style={{ fontSize: "24px" }}className="bi bi-person-fill"></i>
+                            </InputLeftAddon>
+                            <Input type="text" placeholder="Fullname" />
+                            <InputRightAddon style={{ paddingLeft: "5px", paddingRight: "5px" }}>
+                                <i style={{ fontSize: "24px" }} className="bi bi-info-circle"></i>
+                            </InputRightAddon>
+                        </InputGroup>
+
+                        <InputGroup>
+                            <InputLeftAddon style={{ paddingLeft: "5px", paddingRight: "5px" }}>
+                                <i style={{ fontSize: "24px" }} className="bi bi-person-circle"></i>
+                            </InputLeftAddon>
+                            <Input type="text" placeholder="Username" />
+                            <InputRightAddon style={{ paddingLeft: "5px", paddingRight: "5px" }}>
+                                <i style={{ fontSize: "24px" }} className="bi bi-info-circle"></i>
+                            </InputRightAddon>
+                        </InputGroup>
+
+                        <InputGroup>
+                            <InputLeftAddon style={{ paddingLeft: "5px", paddingRight: "5px" }}>
+                                <i style={{ fontSize: "24px" }} className="bi bi-envelope"></i>
+                            </InputLeftAddon>
+                            <Input type="text" placeholder="Email" />
+                            <InputRightAddon style={{ paddingLeft: "5px", paddingRight: "5px" }}>
+                                <i style={{ fontSize: "24px" }} className="bi bi-info-circle"></i>
+                            </InputRightAddon>
+                        </InputGroup>
+
+                        <InputGroup>
+                            <InputLeftAddon style={{ paddingLeft: "5px", paddingRight: "5px" }}>
+                                <i style={{ fontSize: "24px" }} className="bi bi-key"></i>
+                            </InputLeftAddon>
+                            <Input type="password" placeholder="Password" />
+                            <InputRightAddon style={{ paddingLeft: "5px", paddingRight: "5px" }}>
+                                <i style={{ fontSize: "24px" }} className="bi bi-info-circle"></i>
+                            </InputRightAddon>
+                        </InputGroup>
+
+                        <Checkbox name="remember">Remember Me</Checkbox>
+
+                        <Text fontSize="md">
+                            Forgot Password?
+                            <Link style={{ color: "teal", fontWeight: "bold" }} to="/forgot-password">Reset</Link>
+                        </Text>
+
+                        <Button>Sign In</Button>
+                    </Stack>
+                </form>
+            </div>
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App';
 import "./assets/scss/main.scss";
+import theme from './theme/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
