371c5a93d3570f1e9a0460235718104841056056
Foster <72973015+Foster0123@users.noreply.github.com>
2023-02-24 14:50:41 +0530

    Blaze Todo Page Development In Progress + Height Adjusted

--- client/src/components/data/data.ts
+++ client/src/components/data/data.ts
@@ -6,5 +6,7 @@ export const todos: any = [
     { id: 5, header: "Do-something", content: 'Task-5 is crazy long and hard to understand too since its not clearly explained enough' },
     { id: 6, header: "Do-something", content: 'Task-5 is crazy long and hard to understand too since its not clearly explained' },
     { id: 7, header: "Do-something", content: 'Task-5 is crazy long and hard to understand too since enough' },
-    { id: 8, header: "Do-something", content: 'Task-5 is crazy long and hard to understand too since its not clearly' }
+    { id: 8, header: "Do-something", content: 'Task-5 is crazy long and hard to understand too since its not clearly' },
+    { id: 9, header: "Do-something", content: 'Task-5 is crazy long and hard to understand too since its not clearly' },
+    { id: 100, header: "Do-something", content: 'Task-5 is crazy long and hard to understand too since its not clearly' }
 ]
