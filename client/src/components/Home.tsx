d32ddffcd2736de9c485f7b59455ed83df4fc406
Foster <72973015+Foster0123@users.noreply.github.com>
2023-03-06 17:25:24 +0530

    Blaze Index Page Deveopment In Progress

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -5,19 +5,25 @@ import { useEffect, useState } from "react";
 import Footer from "./shared/Footer";
 
 const HomePage = () => {
-    const [ data, setData ] = useState(null)
+    const [data, setData] = useState(null)
     // useEffect(() => {
     //     fetch("http://localhost:3000")
     //     .then(result => result.json())
     //     .then((result: any) => setData(result.error))
     //     .catch((err) => console.error(err))
     // }, [])
-    return(
+    return (
         <div className="index-page" style={{ backgroundImage: `url(${background})` }}>
             <NavBar />
-            <section className="index-image-section">
-                <img className="index-logo" src={logo}  alt="App Logo"/>
-            </section>
+            <div className="intro-section">
+                <section>
+                    <h1>Its Time To Finish Your Unfinished Goals <strong>Blazingly</strong> Fast.</h1>
+                </section>
+                <section className="index-image-section">
+                    <img className="index-logo" src={logo} alt="App Logo" />
+                </section>
+            </div>
+            
             <Footer />
         </div>
     )
